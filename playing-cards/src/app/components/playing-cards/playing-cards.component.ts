28e385d8a9fc3cea0725863d29b2aef1d56d60cb
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2024-09-23 15:37:11 +0000

    Signal Input

--- playing-cards/src/app/components/playing-cards/playing-cards.component.ts
+++ playing-cards/src/app/components/playing-cards/playing-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input, InputSignal } from '@angular/core';
 import { Monster } from '../../model/monster.model';
 
 @Component({
@@ -10,7 +10,8 @@ import { Monster } from '../../model/monster.model';
 })
 export class PlayingCardsComponent {
 
-  // permet de déclarer une propriété de la classe qui sera utilisée dans le template du composant parent
+  /*
+  // @Input : permet de déclarer une propriété de la classe qui sera utilisée dans le template du composant parent
   @Input({
     // required: true, // la propriété est obligatoire
     alias : "my-monster", // alias de la propriété dans le template du composant parent
@@ -19,5 +20,16 @@ export class PlayingCardsComponent {
       return value; // on retourne la valeur transformée
     } // fonction de transformation de la valeur de la propriété
   }) 
-  monster : Monster = new Monster();
+
+  monster : Monter = new Monster(); // propriété de la classe de type Monster
+    */
+  
+  // Utilisation de la directive InputSignal pour déclarer une propriété de la classe qui sera utilisée dans le template du composant parent
+  monster : InputSignal<Monster> = input(new Monster(), {
+    alias : "my-monster", // alias de la propriété dans le template du composant parent
+    transform : (value : Monster) => {
+      value.hp = value.hp / 2; // on divise par 2 les points de vie du monstre
+      return value; // on retourne la valeur transformée
+    } // fonction de transformation de la valeur de la propri
+  });
 }
